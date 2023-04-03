f714bf222f11ad5faa429fffb59d0af33bec86dc
alswooy <jjang990312@naver.com>
2023-04-03 18:22:00 +0900

    feat : [003 - LOGIN - CUSTOMER] 커스터머에 대한 로그인 구현
    JWT의 내부 암호화
    /customer/*에 대한 token filter 적용

--- user-api/src/main/java/com/zerobase/cms/user/UserApplication.java
+++ user-api/src/main/java/com/zerobase/cms/user/UserApplication.java
@@ -1,13 +1,14 @@
 package com.zerobase.cms.user;
 
-import com.zerobase.cms.user.config.FeignConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ServletComponentScan
 @EnableFeignClients
 @SpringBootApplication
 @EnableJpaAuditing
